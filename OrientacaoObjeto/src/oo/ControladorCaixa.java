91cdc49bce75ef1895308db97373569e7bd34637
Milena Gomes <milena.gomes.silva@everis.com>
2020-10-09 15:36:16 -0300

    Interface com os métodos da caixa de som

---
+++ OrientacaoObjeto/src/oo/ControladorCaixa.java
@@ -0,0 +1,13 @@
+package oo;
+
+public interface ControladorCaixa {
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void aumentarSom();
+    public abstract void diminuirSom();
+    public abstract void ativarMudo();
+    public abstract void desativarMudo();
+    public abstract void play();
+    public abstract void pausar();
+
+}
